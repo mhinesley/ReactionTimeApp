75f4f3e2b368c05d7dda91a91d741068a313047a
Matt Hinesley <Matt.Hinesley@etsprayers.com>
2025-01-26 02:31:13 -0500

    added files throuh tutorial

---
+++ preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron/renderer')
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system'),
+  
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+  // we can also expose variables, not just functions
+})
